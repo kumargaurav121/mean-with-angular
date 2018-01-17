427d28c4e6d143a92953bb5269f44dec640837c5
unknown <kumargaurav9013@gmail.com>
2018-01-17 11:39:21 +0530

    Initial Commit 2

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+
+app.get('*', (req, res) => {
+    res.send('<h1>Hello World</h1>');
+})
+
+app.listen(port, () => {
+    console.log(`App is Running on port ${port}`);
+})
